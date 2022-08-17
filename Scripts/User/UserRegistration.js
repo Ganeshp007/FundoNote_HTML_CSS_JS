183dcdd95cba5882749e39115521dbd27b571db7
Ganeshp007 <potdart40.gp@gmail.com>
2022-08-17 22:44:56 +0530

    Added: UC2_UserLogin Page

--- Scripts/User/UserRegistration.js
+++ Scripts/User/UserRegistration.js
@@ -2,17 +2,13 @@ function show(){
 
   var password= document.getElementById('pass');
   var confirm= document.getElementById('Cpass');
-  var image= document.getElementById('eye');
 
   if (password.type==="password",confirm.type==="password") {
     password.type="text";
     confirm.type="text";
-    image.setAttribute('src', '../../Assets/User/eyeshow.png');
-
   }
   else if (password.type==="text",confirm.type==="text"){
     password.type="password";
     confirm.type="password";
-    image.setAttribute('src', '../../Assets/User/eyehide.png');
   }
 }
